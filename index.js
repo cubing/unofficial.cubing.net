c7f84ac3c0cb4c7be21131cc7854ec536bc5fe32
Lucas Garron <code@garron.net>
2023-07-28 20:32:08 -0400

    ✨color✨

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-document.querySelector("#toggle-attempts").addEventListener("click", () => {
+document.querySelector("#toggle-attempts")?.addEventListener("click", () => {
   document.body.classList.toggle("toggle-attempts");
 })
+
+document.body.classList.add([
+  "blue",
+  "orange",
+  "red",
+  "green"
+][Math.floor(Math.random()*4)])
