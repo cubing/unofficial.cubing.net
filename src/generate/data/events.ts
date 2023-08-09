d2a4989f9502af5d4ddcb1569df05aa479134eee
Lucas Garron <code@garron.net>
2023-08-09 09:06:50 +0000

    Apply full event names.

--- src/generate/data/events.ts
+++ src/generate/data/events.ts
@@ -24,95 +24,95 @@ export const events: Record<string, EventMetadata> = {
     cubingIconSkew: true,
   },
   "222_bld": {
-    fullName: "222_bld",
+    fullName: "2x2x2 Blindfolded",
     team: false,
     cubingIconClass: "unofficial-222bf",
   },
   "222_oh": {
-    fullName: "222_oh",
+    fullName: "2x2x2 One-Handed",
     team: false,
     cubingIconClass: "event-222",
     cubingIconSkew: true,
   },
   "333_match_the_scramble": {
-    fullName: "333_match_the_scramble",
+    fullName: "3x3x3 Match The Scramble",
     team: false,
     cubingIconClass: "unofficial-333mts",
   },
   "333_mirror_blocks": {
-    fullName: "333_mirror_blocks",
+    fullName: "3x3x3 Mirror Blocks",
     team: false,
     // cubingIconClass: "333-mirror-blocks", // TODO: https://github.com/cubing/icons/issues/102
     cubingIconClass: "333",
     cubingIconSkew: true, // TODO: https://github.com/cubing/icons/issues/102
   },
   "333_mirror_blocks_bld": {
-    fullName: "333_mirror_blocks_bld",
+    fullName: "3x3x3 Mirror Blocks Blindfolded",
     team: false,
     // cubingIconClass: "333-mirror-blocks", // TODO: https://github.com/cubing/icons/issues/102
     cubingIconClass: "333",
     cubingIconSkew: true,
   },
   "333_no_inspection": {
-    fullName: "333_no_inspection",
+    fullName: "3x3x3 No Inspection",
     team: false,
     cubingIconClass: "event-333",
     cubingIconSkew: true,
   },
   "333_siamese": {
-    fullName: "333_siamese",
+    fullName: "3x3x3 Siamese Cube",
     team: false,
     cubingIconClass: "event-333",
     cubingIconSkew: true,
   },
   "333_speed_bld": {
-    fullName: "333_speed_bld",
+    fullName: "3x3x3 Speed Blindfolded",
     team: false,
     cubingIconClass: "event-333bf",
     cubingIconSkew: true,
   },
   rubiks_360: {
-    fullName: "rubiks_360",
+    fullName: "Rubik's 360",
     team: false,
     cubingIconClass: "event-333",
     cubingIconSkew: true,
   },
   magic_oh: {
-    fullName: "magic_oh",
+    fullName: "Magic One-Handed",
     team: false,
     cubingIconClass: "event-magic",
     cubingIconSkew: true,
   },
   rainbow_cube: {
-    fullName: "rainbow_cube",
+    fullName: "Rainbow Cube",
     team: false,
     cubingIconClass: "event-skewb",
     cubingIconSkew: true,
   },
   relay_222_333_444: {
-    fullName: "relay_222_333_444",
+    fullName: "Relay — 2x2x2, 3x3x3, 4x4x4",
     team: false,
     cubingIconClass: "unofficial-234relay",
   },
   relay_333_333_333: {
-    fullName: "relay_333_333_333",
+    fullName: "Relay — Three 3x3x3 Cubes",
     team: false,
     cubingIconClass: "event-333",
     cubingIconSkew: true,
   },
   skewb: {
-    fullName: "skewb",
+    fullName: "Skewb",
     team: false,
     cubingIconClass: "event-skewb",
   },
   snake: {
-    fullName: "snake",
+    fullName: "Snake",
     team: false,
     cubingIconClass: "event-333",
     cubingIconSkew: true,
   },
   square1_bld: {
-    fullName: "square1_bld",
+    fullName: "Square-1 Blindfolded",
     team: false,
     cubingIconClass: "event-sq1",
     cubingIconSkew: true,
@@ -120,25 +120,6 @@ export const events: Record<string, EventMetadata> = {
 };
 
 export type EventID = keyof typeof events;
-
-// "longName": "Relay — 2x2x2, 3x3x3, 4x4x4"
-// "longName": "Rubik's 360"
-// "longName": "6x6x6 Cube"
-// "longName": "7x7x7 Cube"
-// "longName": "Rainbow Cube"
-// "longName": "Skewb"
-// "longName": "3x3x3 Siamese Cube"
-// "longName": "Snake"
-// "longName": "3x3x3 Mirror Blocks"
-// "longName": "3x3x3 No inspection"
-// "longName": "Relay — Three 3x3x3 Cubes"
-// "longName": "3x3x3 Speed Blindfolded"
-// "longName": "3x3x3 Match the Scramble"
-// "longName": "2x2x2 One-Handed"
-// "longName": "Magic One-Handed"
-// "longName": "2x2x2 Blindfolded"
-// "longName": "Square-1 Blindfolded"
-// "longName": "3x3x3 Mirror Blocks Blindfolded"
 
 export function addIconClasses(elem: HTMLElement, eventID: EventID): void {
   elem.classList.add("cubing-icon");
